cfa898d62699f45fc31aebf7278f2eca947e3c59
Wergugy <wergugy@gmail.com>
2022-09-28 04:51:05 -0700

    Finish the project
    is good for now but i may touch up the ui in the
    future. and maybe aim for those extra credits.
    seems a fun css trick to learn.

--- scripts.js
+++ scripts.js
@@ -1,31 +1,43 @@
-const gridBox = document.querySelector('#grid-container');
-
 function getArea() {
     const area = document.querySelector('#size').value;
     return area;
 };
 
-const gen = document.querySelector('.gen');
-gen.addEventListener('click', build);
-
-
 function build() {
-    gridBox.style.cssText += `grid-template-columns: repeat(${getArea()}, 1fr);
-    grid-template-rows: repeat(${getArea()}, 1fr);`;
-    for (i = 0; i < (getArea() ** 2); i++) {
-        const pix = document.createElement('div');
-        pix.classList.toggle('pix');
-        pix.addEventListener('mouseover', draw, {once: true});
-        gridBox.appendChild(pix);
-    };
+    if ((getArea()) > 100) {
+        warn.textContent = 'Pick a number less than 100!!!';
+        document.querySelector('body').insertBefore(warn, gridBox);
+    }
+    else {
+        gridBox.style.cssText += `grid-template-columns: repeat(${getArea()}, 1fr);
+            grid-template-rows: repeat(${getArea()}, 1fr);`;
+        warn.textContent = '';
+        for (i = 0; i < (getArea() ** 2); i++) {
+            const pix = document.createElement('div');
+            pix.classList.toggle('pix');
+            pix.addEventListener('mouseover', draw, { once: true });
+            gridBox.appendChild(pix);
+        };
+    }
 }
 
-
-
-
-
 function draw(pix) {
-        pix.target.classList.toggle('drawn');
-        pix.target.stopPropagation();
+    pix.target.classList.toggle('drawn');
 };
 
+function reset() {
+    document.querySelectorAll('.pix').forEach((pix) => {
+        pix.parentNode.removeChild(pix);
+    });
+    const input = document.querySelector('#size');
+    input.focus();
+    input.value = null;
+}
+
+const gen = document.querySelector('.gen');
+gen.addEventListener('click', build, { capture: true });
+
+const gridBox = document.querySelector('#grid-container');
+
+document.querySelector('.res').addEventListener('click', reset);
+const warn = document.createElement('span');
